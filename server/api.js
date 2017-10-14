e270b571143612602989062e843874e52e683bfd
Andrew Niekamp <andrew.f.niekamp@gmail.com>
2017-10-14 08:37:03 -0400

    Fixed student creation association with campus

--- server/api.js
+++ server/api.js
@@ -3,22 +3,22 @@ const api = require('express').Router()
 const db = require('../db')
 
 // If you aren't getting to this object, but rather the index.html (something with a joke) your path is wrong.
-	// I know this because we automatically send index.html for all requests that don't make sense in our backend.
-	// Ideally you would have something to handle this, so if you have time try that out!
-api.get('/hello', (req, res) => res.send({hello: 'world'}))
+// I know this because we automatically send index.html for all requests that don't make sense in our backend.
+// Ideally you would have something to handle this, so if you have time try that out!
+api.get('/hello', (req, res) => res.send({ hello: 'world' }))
 
 api.get('/campuses', (req, res) => {
 	db.model('campus').findAll()
-	.then( campuses => {
-		res.send(campuses);
-	})
+		.then(campuses => {
+			res.send(campuses);
+		})
 })
 
 api.get('/campuses/:id', (req, res) => {
-	db.model('campus').findById(req.params.id, {include: [{ model: db.model('student')}]})
-	.then( campus => {
-		res.send(campus);
-	})
+	db.model('campus').findById(req.params.id, { include: [{ model: db.model('student') }] })
+		.then(campus => {
+			res.send(campus);
+		})
 })
 
 api.post('/campuses/add', (req, res) => {
@@ -26,36 +26,33 @@ api.post('/campuses/add', (req, res) => {
 		name: req.body.name,
 		imageURL: req.body.imageURL
 	})
-	.then( campus => {
-		res.send(campus);
-	})
+		.then(newCampus => {
+			res.send(newCampus);
+		})
 })
 
 api.get('/students', (req, res) => {
 	db.model('student').findAll()
-	.then( student => {
-		res.send(student);
-	})
+		.then(student => {
+			res.send(student);
+		})
 })
 
 api.get('/students/:id', (req, res) => {
-	db.model('student').findById(req.params.id, {include: [{ model: db.model('campus')}]})
-	.then( student => {
-		res.send(student);
-	})
+	db.model('student').findById(req.params.id, { include: [{ model: db.model('campus') }] })
+		.then(student => {
+			res.send(student);
+		})
 })
 
 api.post('/students/add', (req, res) => {
-	db.model('campus').findById(req.body.campusId)
-	.then( foundCampus => {
-		db.model('student').create({
-			name: req.body.name,
-			email: req.body.email,
-			campus: foundCampus
-		})
-		.then( campus => {
-			res.send(campus);
-		})
+	db.model('student').create({
+		name: req.body.name,
+		email: req.body.email,
+		campusId: req.body.campusId
+	})
+	.then(newStudent => {
+		res.send(newStudent);
 	})
 })
 
